f22c3d6601b3f2eeb489489fa85a920f60990234
Проданюк Владимир Анатольевич <v.prodaniuk@office.opticom.local>
2019-10-21 19:43:42 +0300

    first commit

---
+++ app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
@@ -0,0 +1,19 @@
+package com.hfad.mymessenger;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class CreateMessageActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+
+    //Вызвать onSendMessage() при щелчке на кнопке
+    public void onSendMessage(View view) {
+
+    }
+
+}
